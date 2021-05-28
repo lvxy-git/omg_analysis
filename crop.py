feedc80942086bccf82fdd543fbc29d038415341
吕旭阳 <lvxy@yealink.com>
2021-05-28 11:41:30 +0800

    ‘sss’

--- crop.py
+++ crop.py
@@ -56,7 +56,7 @@ def classify_hist_with_split(image1, image2, size=(256, 256)):
     sub_data = sub_data / 3
     return sub_data
 
-def phash(img):
+def phash(image):
     # 感知哈希算法
     # 缩放32*32
     img = cv2.resize(img, (32, 32))  # , interpolation=cv2.INTER_CUBIC
@@ -78,6 +78,23 @@ def phash(img):
                 hash.append(0)
     return hash
 
+def dhash(img):
+    # 差值哈希算法
+    # 缩放8*8
+    img = cv2.resize(img, (9, 8))
+    # 转换灰度图
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hash_str = ''
+    # 每行前一个像素大于后一个像素为1，相反为0，生成哈希
+    for i in range(8):
+        for j in range(8):
+            if gray[i, j] > gray[i, j+1]:
+                hash_str = hash_str+'1'
+            else:
+                hash_str = hash_str+'0'
+    return hash_str
+
+
 
 
 def campHash(hash1, hash2):
@@ -95,13 +112,15 @@ def campHash(hash1, hash2):
 for i in range(len(ultimate_list)):
     skill_hash = phash(ultimate_list[i])
 
-    maxHash = 0
+    maxHash = 20
     real = ''
     for skill in os.listdir(r'.\skill_image'):
         skill_image = cv2.imread('.\skill_image\\'+skill)
-        compare = classify_hist_with_split(skill_image, ultimate_list[i])
-        #compare = campHash(skill_hash, a)
-        if compare > maxHash:
+        # compare = classify_hist_with_split(skill_image, ultimate_list[i])
+        compare = campHash(skill_hash, phash(skill_image))
+        if skill == 'razor_eye_of_the_storm.jpg':
+            print('c='+str(compare))
+        if compare < maxHash:
             maxHash = compare
             real = skill
 

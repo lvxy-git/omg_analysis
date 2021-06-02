2320b3bb9062bda09cf88fc3dc663e6aec063bca
吕旭阳 <lvxy@yealink.com>
2021-06-02 16:32:56 +0800

    提交skill_rank.py

--- skill_rank.py
+++ skill_rank.py
@@ -5,9 +5,10 @@ import time
 import logging
 import requests
 
+
 def get_skill_rank():
-    if not os.path.exists('skill_rank.json') \
-            or time.time() - os.path.getmtime('skill_rank.json') > 604800: # 文件修改时间大于一周
+    if time.time() - os.path.getmtime('skill_rank.json') > 604800 \
+            or not os.path.exists('skill_rank.json'):  # 文件修改时间大于一周或不存在
         session_request = requests.session()
         login_url = 'https://dota2omg.com/login'
         login_result = session_request.post(
@@ -26,13 +27,12 @@ def get_skill_rank():
             rate_result = session_request.get(rate_url)
             # print(rate_result.text)
             with open('skill_rank.json', 'w') as fp:
-                j = json.dumps(json.loads(rate_result.content.decode('utf-8'))['rows'],indent=4)
+                j = json.dumps(json.loads(rate_result.content.decode('utf-8'))['rows'], indent=4)
                 fp.write(j)
     with open('skill_rank.json', 'r', encoding='utf8') as fp:
         json_data = json.load(fp)
         return json_data
 
 
-
-if  __name__ == '__main__':
+if __name__ == '__main__':
     print(get_skill_rank())
